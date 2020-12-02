8dedd7807b52737bf06e44416763b139e518229e
Adam Shepherd <ashepherd@whoi.edu>
2020-12-02 15:11:04 -0500

    adds file loader

--- code/app.py
+++ code/app.py
@@ -4,15 +4,21 @@ import logging
 import os
 import pwd
 from rdflib import Graph, Literal, Namespace, RDF, URIRef
+from rdflib.util import guess_format
+import re
 import requests
 import shutil
 import simplejson as json
-import sparql
+from SPARQLWrapper import SPARQLWrapper, POST, BASIC, DIGEST
 import traceback
+import unicodedata
+from urllib.parse import urlparse
 from werkzeug.exceptions import HTTPException
 import yaml
 
-app = Flask(__name__, static_url_path='', static_folder='/web/static')
+app = Flask(__name__, static_url_path='')
+
+RDF_DOWNLOAD_PATH = '/web/rdf'
 
 ### LOCAL ###
 def write_ready_file(path):
@@ -31,6 +37,15 @@ def local_check_dump_path(path):
     lock_file = path + os.path.sep + 'locked'
     return os.path.exists(lock_file)
 
+def download_url(url, file, **kwargs):
+    print(kwargs)
+    # https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
+    with requests.get(url, stream=True, **kwargs) as r:
+        r.raise_for_status()
+        with open(file, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
 def local_download_void_data_dump(url, path, user=None, group=None):
     data_dump_filename = url.rsplit('/', 1)[1]
     if not os.path.exists(path):
@@ -41,17 +56,7 @@ def local_download_void_data_dump(url, path, user=None, group=None):
     file = path + os.path.sep + data_dump_filename
 
     # https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
-    with requests.get(url, stream=True) as r:
-        #with open(file, 'wb') as f:
-        #    shutil.copyfileobj(r.raw, f)
-        r.raise_for_status()
-        with open(file, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk:
-                f.write(chunk)
-
+    download_url(url, file)
 
     if user is not None:
         uid = pwd.getpwnam(user).pw_uid
@@ -63,7 +68,18 @@ def local_download_void_data_dump(url, path, user=None, group=None):
 
 
 ### UTILS ###
-def read_config(graph):
+def slugify(filename):
+    """
+    Convert to ASCII. Convert spaces to hyphens.
+    Remove characters that aren't alphanumerics, underscores, or hyphens.
+    Convert to lowercase. Also strip leading and trailing whitespace.
+    """
+    value = str(filename)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
+    return re.sub(r'[-\s]+', '-', value)
+
+def read_config():
     # Read the configuration so that we can support on the fly
     with open('config.yaml', 'r') as yamlfile:
         cfg = yaml.safe_load(yamlfile)
@@ -119,7 +135,122 @@ def handle_exception(e):
 @app.route('/logs/<path:path>', methods=['GET'])
 def view_log_file(path):
     app.logger.debug('PATH: ' + path)
-    return send_from_directory('/logs', path, mimetype='text/plain')
+    return send_from_directory('/logs', path, mimetype='text/plain', cache_timeout=-1)
+
+@app.route('/rdf/turtle/<path>', methods=['GET'])
+def view_rdf_turtle_file(path):
+    app.logger.debug('RDF: ' + path)
+    return send_from_directory('/web/rdf', path, mimetype='text/turtle', cache_timeout=-1)
+
+@app.route('/file-graph-loader', methods=['GET'])
+def file_graph_load():
+    if 'BASE_URL' not in os.environ:
+        abort(404, description="No base URL defined")
+    base_url = os.environ['BASE_URL']
+
+    graph = request.args.get('graph')
+    if graph is None:
+        abort(404, description="Graph not provided")
+
+    triplestore = request.args.get('triplestore')
+    if triplestore is None:
+        abort(404, description="Triplestore not provided")
+
+    file = request.args.get('file')
+    if file is None:
+        abort(404, description="File not provided")
+
+    rdf_format = request.args.get('format')
+    if rdf_format is None:
+        abort(404, description="RDF Format not provided")
+
+    # get the latest configuration
+    cfg = read_config()
+    if 'triplestore' not in cfg:
+        abort(404, description="No triplestore configuration")
+    ts_cfg = cfg['triplestore'].get(triplestore, None)
+    if ts_cfg is None:
+        abort(404, description="Triplestore configuration not found")
+
+    if 'endpoint' not in ts_cfg:
+        abort(404, description="No triplestore endpoint")
+
+    file_kwargs = None
+    file_server = request.args.get('server')
+    if file_server is not None:
+        if 'dpp2prov' == file_server:
+            file_kwargs = {
+                headers:{
+                    'X-API-KEY': os.environ['dpp2prov_api_key']
+                }
+            }
+
+    #if 'path' not in ts_cfg:
+    ##    abort(404, description="No triplstore dump path in configuration")
+    #dump_path = ts_cfg['path']
+    dump_path = RDF_DOWNLOAD_PATH
+
+    # Download file
+    filename = slugify('__'.join(file.split('/')[2:]))
+    destination = os.path.join(dump_path, filename)
+    app.logger.debug('Download RDF file: ' + destination)
+    if file_kwargs is not None:
+        download_url(file, destination, **file_kwargs)
+    else:
+        download_url(file, destination)
+
+    # Convert to turtle and remove original file
+    if 'turtle' != rdf_format:
+        # Parse original file
+        rdf = Graph()
+        app.logger.info('Parsing RDF at: ' + destination)
+        rdf.parse(destination, format=rdf_format)
+        # Prepare a turtle file
+        filename += '.ttl'
+        ttl_destination = os.path.join(dump_path, filename)
+        app.logger.debug('Turtle file: ' + ttl_destination)
+        # Convert RDF to turtle
+        rdf.serialize(destination=ttl_destination, format='turtle')
+        # Delete original file
+        os.remove(destination)
+        destination = ttl_destination
+
+    # Load Turtle into Triplestore
+    load_url = base_url + '/rdf/turtle/' + filename
+    #load_url = 'https://www.w3.org/ns/dcat2.ttl'
+    app.logger.debug('LOAD: ' + load_url)
+    app.logger.debug('Endpoint: ' + ts_cfg['endpoint'])
+    sparql = SPARQLWrapper(ts_cfg['endpoint'])
+    auth = ts_cfg.get('auth', 'none')
+    if 'none' != auth:
+        user_var = triplestore+'_user'
+        if user_var not in os.environ:
+            abort(404, description="No triplestore username")
+        pswd_var = triplestore+'_pswd'
+        if pswd_var not in os.environ:
+            abort(404, description="No triplestore password")
+        if 'basic' == auth:
+            sparql.setHTTPAuth(BASIC)
+        elif 'digest' == auth:
+            sparql.setHTTPAuth(DIGEST)
+        sparql.setCredentials(os.environ[user_var], os.environ[pswd_var])
+    sparql.setMethod(POST)
+    stmt = 'CLEAR GRAPH <' + graph + '> LOAD <' + load_url + '> INTO GRAPH <' + graph + '>'
+    app.logger.info(stmt)
+    sparql.setQuery(stmt)
+
+    results = sparql.query()
+    res = results.response.read()
+
+    # Delete destination after loading
+    os.remove(destination)
+
+    # Send response
+    json = {
+        'response': res
+    }
+    response = make_response(json, 200)
+    return response_json(response)
 
 
 @app.route('/void-graph-loader', methods=['GET'])
@@ -129,11 +260,11 @@ def virtuoso_graph_load():
         abort(404, description="Graph not provided")
 
     triplestore = request.args.get('triplestore')
-    if graph is None:
+    if triplestore is None:
         abort(404, description="Triplestore not provided")
 
     # get the latest configuration
-    cfg = read_config(graph)
+    cfg = read_config()
     if 'void' not in cfg:
         abort(404, description="No VoID configuration")
     graph_cfg = cfg['void'].get(graph, None)
